628f962591c43a77ab4fe79b95cee4d940ec92f2
jingj_liu@hotmail.com <vdergow@hotmail.com>
2023-10-18 00:02:55 +0800

    feat: 完善resolve和reject队列 & then方法 & 支持链式调用

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 class MyPromise {
+	static PENDING = "pending"
+	static RESOLVED = "resolved"
+	static REJECTED = "rejected"
+
 	constructor(executor) {
 		// pending, resolve, reject
-		this.status = "pending"
+		this.status = MyPromise.PENDING
 		this.value = null
 		this.reason = null
 
-		this.onFulfilled = null
-		this.onRejected = null
+		// this.onFulfilled = null
+		// this.onRejected = null
+
+		this.resolveQueues = []
+		this.rejectQueues = []
 
 		let resolve = (value) => {
-			this.value = value
-			this.onFulfilled && this.onFulfilled(this.value)
+			if (this.status == MyPromise.PENDING) {
+				this.status = MyPromise.RESOLVED
+				this.value = value
+				// this.onFulfilled && this.onFulfilled(this.value)
+				this.resolveQueues && this.resolveQueues.forEach((fn) => fn(this.value))
+			}
 		}
 
 		let reject = (error) => {
-			this.reason = error
-			this.onRejected && this.onRejected(this.reason)
+			if (this.status == MyPromise.PENDING) {
+				this.status = MyPromise.REJECTED
+				this.reason = error
+				// this.onRejected && this.onRejected(this.reason)
+				this.rejectQueues && this.rejectQueues.forEach((fn) => fn(this.reason))
+			}
 		}
 
 		if (typeof executor == "function") {
@@ -31,17 +46,66 @@ class MyPromise {
 
 	// then函数，传入onFulFilled和onRejected方法
 	then(onFulfilled, onRejected) {
-		this.onFulfilled = onFulfilled
-		this.onRejected - onRejected
+		// this.onFulfilled = onFulfilled
+		// this.onRejected - onRejected
+
+		// 首先判断两个参数是否为函数类型，因为这两个参数是可选参数
+		// 当参数不是函数类型时，需要创建一个函数赋值给对应的参数
+		// 这也就实现了 透传
+		onFulfilled =
+			typeof onFulfilled === "function" ? onFulfilled : (value) => value
+		onRejected =
+			typeof onRejected === "function"
+				? onRejected
+				: (reason) => {
+						throw reason
+				  }
+
+		let p1 = new Promise((resolve, reject) => {
+			// 当状态是等待态的时候，需要将两个参数塞入到对应的回调数组中
+			// 当状态改变之后，在执行回调函数中的函数
+			if (this.status === MyPromise.PENDING) {
+				this.resolveQueues.push((value) => {
+          let x = onFulfilled(value);
+          resolve(x);
+        })
+				this.rejectQueues.push((reason) => {
+          let x = onRejected(reason);
+          resolve(x);
+        })
+			}
+
+			// 状态是成功态，直接就调用 onFulfilled 函数
+			if (this.status === MyPromise.RESOLVED) {
+				let x = onFulfilled(this.value)
+        resolve(x);
+			}
+
+			// 状态是成功态，直接就调用 onRejected 函数
+			if (this.status === MyPromise.REJECTED) {
+				let x = onRejected(this.reason)
+        reject && reject(x);
+			}
+		})
+    return p1;
 	}
 }
 
 let p1 = new MyPromise((resolve, reject) => {
 	resolve("test")
-	setTimeout(() => {}, 0)
 })
 p1.then((res) => {
 	console.log(res)
+  return 't1';
 }).then((res) => {
 	console.log(res)
+  return 't2';
+})
+
+let p2 = new MyPromise((resolve, reject) => {
+	resolve("test")
+})
+p2.then(12).then((res) => {
+	console.log(res)
+  return 't2';
 })
